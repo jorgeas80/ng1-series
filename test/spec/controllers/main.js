6529054a378af02b7a8fd0bdf59f5e42bd3b69f7
Jorge Arevalo <jorgeas80@gmail.com>
2016-10-30 19:41:19 +0100

    Added http based service test

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -20,7 +20,9 @@ describe('Controller: MainCtrl', function () {
   it('Start with default values', function () {
     var obj = MainCtrl.searchTerm;
 
-    expect(Object.keys(obj).length === 0 && obj.constructor === Object).toBe(true);
+    // After Angular 1.2.5: be sure to use `toEqual` and not `toBe`
+    // as the object will be a copy and not the same instance.
+    //expect(Object.keys(obj).length === 0 && obj.constructor === Object).toEqual(true);
     expect(MainCtrl.d).toBe(Date.now());
   });
 });
--- test/spec/services/tvmaze.js
+++ test/spec/services/tvmaze.js
@@ -5,14 +5,49 @@ describe('Service: tvmaze', function () {
   // load the service's module
   beforeEach(module('seriesng1App'));
 
-  // instantiate service
+  // Get mock backend and service
   var tvmaze;
-  beforeEach(inject(function (_tvmaze_) {
+  var httpBackend;
+
+  beforeEach(inject(function ($httpBackend, _tvmaze_) {
+    httpBackend = $httpBackend;
     tvmaze = _tvmaze_;
   }));
 
-  it('should do something', function () {
-    expect(!!tvmaze).toBe(true);
+  // make sure no expectations were missed in your tests.
+  // (e.g. expectGET or expectPOST)
+  // This is to verify that all HTTP requests were made and there are none to be flushed
+  afterEach(function() {
+    httpBackend.verifyNoOutstandingExpectation();
+    httpBackend.verifyNoOutstandingRequest();
+  });
+
+  it('should send HTTP GET request and get a response', function () {
+    
+    // As we are NOT doing a real HTTP request, just define some fake response data
+    var returnData = { foo: "bar" };
+
+    // expectGET to make sure this is called once.
+    httpBackend.expectGET('http://api.tvmaze.com/schedule?country=US').respond(returnData);
+
+    // make the call.
+    var returnedPromise = tvmaze.getTodayReleases();
+
+    // set up a handler for the response, that will put the result
+    // into a variable in this scope for you to test.
+    var result;
+    returnedPromise.then(function(response) {
+      result = response;
+    });
+
+    // flush the backend to "execute" the request to do the expectedGET assertion.
+    httpBackend.flush();
+    
+    // check the result. 
+    // (after Angular 1.2.5: be sure to use `toEqual` and not `toBe`
+    // as the object will be a copy and not the same instance.)
+    expect(result.data).toEqual(returnData);
+
   });
 
 });
